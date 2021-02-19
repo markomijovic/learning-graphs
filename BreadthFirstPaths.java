e5abc012d651a434d2fdcebefe810baffc414b60
Marko Mijovic <mijovicmarko11@gmail.com>
2021-02-18 17:09:43 -0700

    Breadth First Search

---
+++ BreadthFirstPaths.java
@@ -0,0 +1,28 @@
+import java.util.Queue;
+
+public class BreadthFirstPaths 
+{   
+    // O(E+V)
+    private boolean[] marked;
+    private int[] edgeTo;
+    ...
+    private void bfs(Graph G, int s)
+    {
+        Queue<Integer> q = new Queue<Integer>();
+        q.enqueue(s);
+        marked[s] = true;
+        while (!q.isEmpty())
+        {
+            int v = q.dequeue();
+            for (int w : G.adj(v))
+            {
+                if (!marked(w))
+                {
+                    q.enqueue(w);
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                }
+            }
+        }
+    }
+}
